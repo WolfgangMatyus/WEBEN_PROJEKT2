bf091c60cbf0b565481a1b6d246b1de1107521f4
WolfgangMatyus <wi21b055@technikum-wien.at>
2023-04-02 03:29:27 +0200

    work in progress

--- res/js/mycode.js
+++ res/js/mycode.js
@@ -1,24 +1,38 @@
-//VALIDATION:
-$("#formValidation").validate({
-    rules:{
-        name:{
-            minlength: 2
+
+//-----REGISTRATION--------//
+    //VALIDATION:
+    $("#formValidation").validate({
+        rules:{
+            Vorname:{
+                minlength: 2
+            },
+            email:{
+                email:true
+            }
         },
-        email:{
-            email:true
+        message:{
+            Vorname:{
+                required:"Please enter your name",
+                minlength:"Name at least 2 characters"
+            },
+            Vorname:"Please enter your Name",
+            Emailadresse:"Please enter your email",
+            Adresse:"Please enter your Adress"
+        },
+        
+        submitHandler: function(form) {
+        form.submit();
         }
-    },
-    message:{
-        name:{
-            required:"Please enter your name",
-            minlength:"Name at least 2 characters"
-        },
-        name:"Please enter your email",
-        email:"Please enter your email",
-        Adress:"Please enter your email"
-    },
-    
-    submitHandler: function(form) {
-      form.submit();
-    }
-   });
+    });
+
+   //WRITE JSON:
+   $.ajax
+    ({
+        type: "GET",
+        dataType : 'json',
+        async: false,
+        url: 'http://your.host/save_json.php',
+        data: { data: JSON.stringify(eventsholded) },
+        success: function () {alert("Thanks!"); },
+        failure: function() {alert("Error!");}
+    });
