a55ecc2c83df12cf7391898c25c475b59f4c0c35
rgrohmann <grohmann.rainer@gmx.at>
2023-04-10 22:34:21 +0200

    [BE] Java Spring Framework aufsetzen #8

---
+++ BE/src/main/java/com/backend/controller/CustomerController.java
@@ -0,0 +1,18 @@
+package com.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/customer/")
+public class CustomerController {
+
+    @GetMapping
+    public ResponseEntity<String> getCustomer() {
+        return ResponseEntity.ok("asdf");
+    }
+
+}
+
---
+++ BE/src/main/java/com/backend/security/user/Role.java
@@ -0,0 +1,7 @@
+package com.backend.security.user;
+
+public enum Role {
+
+  CUSTOMER,
+  ADMIN
+}
