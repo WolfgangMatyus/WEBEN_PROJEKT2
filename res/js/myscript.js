8df80fea20613efb8a90b40f57fee73d6a5b06bc
WolfgangMatyus <wi21b055@technikum-wien.at>
2023-04-10 21:37:17 +0200

    sprint1

--- res/js/myscript.js
+++ res/js/myscript.js
@@ -4,27 +4,59 @@
     //VALIDATION: https://jqueryvalidation.org/
     $("#formValidation").validate({
         rules:{
+            Anrede:{
+                required: true,
+                maxlength: 4
+            },
             Vorname:{
+                required: true,
+                minlength: 2
+            },
+            Nachname:{
+                required: true,
+                minlength: 2
+            },
+            Strasse:{
+                required: true,
+            },
+            PLZ:{
+                required: true,
+                digits: true
+            },
+            ORT:{
+                required: true,
                 minlength: 2
             },
             email:{
+                required: true,
                 email:true
-            }
+            },
+            Passwort:{
+                required: true,
+    
+            },
+            Passwort2:{
+                required: true,
+                
+            },
         },
-        message:{
+        messages:{
             Vorname:{
                 required:"Please enter your name",
                 minlength:"Name at least 2 characters"
             },
-            Vorname:"Please enter your Name",
-            Emailadresse:"Please enter your email",
-            Adresse:"Please enter your Adress"
+            email:{
+                email:"Please enter your email"
+            },
+            Adress:{
+                required:"Please enter your Adress"
+            }
         },
         
         submitHandler: function(form) {
-        form.submit();
+          form.submit();
         }
-    });
+       });
 
    //WRITE JSON:
    $.ajax
